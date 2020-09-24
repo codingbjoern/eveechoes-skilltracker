0f4af4f7ae4ec2612682696d9f4d10541e839f42
codingbjoern <bjoern.boyens@deutsche-boerse.com>
2020-09-24 19:50:32 +0200

    added functionality to load data

--- chatbot_skilltracker.php
+++ chatbot_skilltracker.php
@@ -22,10 +22,14 @@
 	// Create the DiscordClient and let the user know how to invite the bot
         $client = new DiscordClient(['token' => $token]);
         $timestampLastMessageFile = $ini['timestampLastMessageFile'];
-        $lastRun = file_get_contents($timestampLastMessageFile);
-
-        $lastMessageTimestamp = $client->channel->getChannelMessages(['channel.id' => 758584661503901697, 'channel.before' => $lastRun])[0]->timestamp->format('Y-m-d H:i:s.u');
-        var_dump($lastMessageTimestamp);
+        if (file_exists($timestampLastMessageFile)) {
+		$lastRun = file_get_contents($timestampLastMessageFile);
+	        $messages = $client->channel->getChannelMessages(['channel.id' => 758584661503901697, 'channel.after' => "$lastRun"]);
+	} else {
+		$messages = $client->channel->getChannelMessages(['channel.id' => 758584661503901697]);
+	}
+	var_dump($messages);
+	$lastMessageTimestamp = $messages[0]->timestamp->format('Y-m-d H:i:s.u');
         if (empty($lastMessageTimestamp)) {
         	echo "nothing todo";
         } else {
