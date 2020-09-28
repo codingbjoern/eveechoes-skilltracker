50357ace1ff78cc7bbdb9fae80174732378aee8d
codingbjoern <bjoern.boyens@deutsche-boerse.com>
2020-09-28 16:12:44 +0200

    changed skilltracker, that it would work with db

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,15 +22,13 @@ class ComposerAutoloaderInit962d3306e732c232defdc75fb55345d4
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit962d3306e732c232defdc75fb55345d4', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit962d3306e732c232defdc75fb55345d4', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
+            require_once __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit962d3306e732c232defdc75fb55345d4::getInitializer($loader));
         } else {
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -133,7 +133,6 @@ class ComposerStaticInit962d3306e732c232defdc75fb55345d4
     public static $classMap = array (
         'ArithmeticError' => __DIR__ . '/..' . '/symfony/polyfill-php70/Resources/stubs/ArithmeticError.php',
         'AssertionError' => __DIR__ . '/..' . '/symfony/polyfill-php70/Resources/stubs/AssertionError.php',
-        'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
         'DivisionByZeroError' => __DIR__ . '/..' . '/symfony/polyfill-php70/Resources/stubs/DivisionByZeroError.php',
         'Error' => __DIR__ . '/..' . '/symfony/polyfill-php70/Resources/stubs/Error.php',
         'Normalizer' => __DIR__ . '/..' . '/symfony/polyfill-intl-normalizer/Resources/stubs/Normalizer.php',
