9ccf5a8db48e2e529bef94ecfb68471026581885
codingbjoern <bjoern.boyens@deutsche-boerse.com>
2020-09-28 15:50:28 +0200

    new version of chatbot

--- chatbot_skilltracker.php
+++ chatbot_skilltracker.php
@@ -1,4 +1,3 @@
-#!/usr/bin/env php
 <?php
 	require_once(__DIR__ . '/vendor/autoload.php');
 
@@ -21,19 +20,68 @@
 
 	// Create the DiscordClient and let the user know how to invite the bot
         $client = new DiscordClient(['token' => $token]);
-        $timestampLastMessageFile = $ini['timestampLastMessageFile'];
-        if (file_exists($timestampLastMessageFile)) {
-		$lastRun = file_get_contents($timestampLastMessageFile);
-	        $messages = $client->channel->getChannelMessages(['channel.id' => 758584661503901697, 'channel.after' => "$lastRun"]);
+        $idLastMessageFile = $ini['idLastMessageFile'];
+        if (file_exists($idLastMessageFile)) {
+		$lastMessageId = (int)file_get_contents($idLastMessageFile);
+	        $messages = $client->channel->getChannelMessages(['channel.id' => 758584661503901697, 'after' => $lastMessageId]);
 	} else {
 		$messages = $client->channel->getChannelMessages(['channel.id' => 758584661503901697]);
 	}
-	var_dump($messages);
-	$lastMessageTimestamp = $messages[0]->timestamp->format('Y-m-d H:i:s.u');
-        if (empty($lastMessageTimestamp)) {
-        	echo "nothing todo";
-        } else {
-        	file_put_contents($timestampLastMessageFile, $lastMessageTimestamp); 
+	if (empty($messages)) {
+		write_log_information("keine Nachrichten zu verarbeiten");
+	} else {
+		foreach ($messages as &$message) {
+			write_log_information("start processing message " . $message->content);
+			if (empty($message->content)) {
+				write_log_information("No content in message");
+			} else {
+				$answer = get_answer($ini, $message);
+				echo $answer;
+			}
+		}
+		$lastMessageId = $messages[0]->id;
+	        if (empty($lastMessageId)) {
+	        	write_log_information("nothing todo");
+	        } else {
+	        	file_put_contents($idLastMessageFile, $lastMessageId); 
+		}
+	}
+	
+	function get_answer ($configuration, $userinput)
+	{
+		$servername = $configuration['servername'];
+		$username = $configuration['username'];
+		$password = $configuration['password'];
+		// Create connection
+		$conn = new mysqli($servername, $username, $password);
+
+		// Check connection
+		if ($conn->connect_error) {
+			$error_message = "Connection failed: " . $conn->connect_error;
+			write_log_information($error_message);
+		  	die($error_message);
+		}
+		write_log_information("Connected successfully");
+		$sql = "SELECT answer FROM question_answers WHERE question = '" . $userinput->content . "'";
+		write_log_information("run query: " . $sql);
+		if($result = $conn->query($sql)) 
+		{
+			var_dump($result);
+			if ($result->num_rows > 0) {
+				// output data of each row
+				while($row = $result->fetch_assoc()) {
+		    			echo "answer: " . $row["answer"] . "<br>";
+		  		}
+			} else {
+		  		echo "0 results";
+			}
+		}
+		$conn->close();
+	}
+	
+	function write_log_information($log_information)
+	{
+		file_put_contents('log_'.date("j.n.Y").'.log', date('l jS \of F Y h:i:s A') . " " . $log_information . "\n",FILE_APPEND);
 	}
 
-
+?>
